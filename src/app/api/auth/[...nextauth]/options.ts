e301dc672a6a065a925360097c8ac3c7d13d834e
shekhar-sharma-111 <shekharsharma7206@gmail.com>
2024-09-29 23:57:30 +0530

    options.ts error solved

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 
 
 import CredentialsProvider from "next-auth/providers/credentials";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,9 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 
-const typingSpeed = 70; 
-const subTypingSpeed = 50; 
-const reverseTypingSpeed = 50; 
 
 const HomePage = () => {
-  const [mainText, setMainText] = useState(' ');
-  const [subText, setSubText] = useState(' ');
-  const mainMessage = "Weelcome to SNEAKTALK";
-  const subMessage = "  This is a place for creativity and exploration.";
+ 
   const messages=[
     {
     id:1,
@@ -48,57 +42,11 @@ const HomePage = () => {
 ]
 
 
-  useEffect(() => {
-    let index = 0;
-
-    const typingInterval = setInterval(() => {
-      if (index < mainMessage.length) {
-        setMainText((prev) => prev + mainMessage.charAt(index));
-        index++;
-      } else {
-        clearInterval(typingInterval);
-        startSubTextTyping();
-      }
-    }, typingSpeed);
-
-    return () => clearInterval(typingInterval);
-  }, []);
-
-  const startSubTextTyping = () => {
-    let index = 0;
-
-    const typingInterval = setInterval(() => {
-      if (index < subMessage.length) {
-        setSubText((prev) => prev + subMessage.charAt(index));
-        index++;
-      } else {
-        clearInterval(typingInterval);
-        reverseSubText(); 
-      }
-    }, subTypingSpeed);
-  };
-
-  const reverseSubText = () => {
-    let index = subMessage.length;
-
-    const reverseInterval = setInterval(() => {
-      if (index > 0) { 
-        setSubText(subMessage.substring(0, index));
-        index--;
-      } else {
-        clearInterval(reverseInterval);
-        setTimeout(() => {
-          setSubText(''); 
-          startSubTextTyping();
-        }, 10); 
-      }
-    }, reverseTypingSpeed);
-  };
-
+ 
   return (<>
     <div className="flex flex-col justify-start items-center h-screen bg-gray-100 p-10">
-      <h1 className="text-5xl font-bold text-gray-800 mb-4">{mainText}</h1>
-      <h2 className="text-3xl font-semibold text-gray-900 mb-10 font-mono">{">"}{subText}{"<"}</h2>
+      <h1 className="text-5xl font-bold text-gray-800 mb-4">Welcome to SneakTalk</h1>
+      <h2 className="text-3xl font-semibold text-gray-900 mb-10 font-mono">{">"}Place to get ananymous messages{"<"}</h2>
       <Carousel className="w-full max-w-xs" plugins={[Autoplay({ delay: 2000 })]}>
   <CarouselContent>
     {messages.map((message, index) => (
